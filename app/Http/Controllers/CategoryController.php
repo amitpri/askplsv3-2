50b95a7d893f5647968d08e91bfb2ec282bd26ac
Amit Priyadarshi <amitpri@gmail.com>
2019-06-10 11:28:10 +0530

    links

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -24,13 +24,20 @@ class CategoryController extends Controller
     	$user_code = $url;
         $categorytype = "Colleges";
 
-        $user = College::where('collegekey','=',$url)->first(['id','collegekey AS user_code', 'name']);
+        $user = College::where('collegekey','=',$url)->first(['id','collegekey AS user_code', 'name' , 'city', 'country' , 'address', 'type' ]);
 
         $topicable = "App\College";
 
 
         $id = $user->id;
         $name = $user->name;
+        $address = $user->address;
+        $city = $user->city;
+        $country = $user->country;  
+        $speciality =  "";
+        $type = "";
+        $qualification =  "";
+        $exp =  "";
          
         $topic = ShowTopicCategory::where('topicable_type', '=', $topicable)->where('topicable_id', '=', $id)->first(['id']);
 
@@ -51,7 +58,20 @@ class CategoryController extends Controller
 
         }
 
-        return view('viewprofiledoctor', compact('id', 'user_code', 'categorytype', 'name'));
+        $topics = DB::select("SELECT  a.`id`,  a.`url` , a.`user_id`,  a.`topic_name`
+                                    , DATE_FORMAT(a.`created_at`, '%d-%b-%Y')  created_at ,
+                                    (case when (a.`anonymous` = 0) then c.`user_code` else '-1' end) as user_code,
+                                    (case when (a.`anonymous` = 0) then c.`name` else '-1' end) as user_name
+                                FROM `topic_categories` a ,  `colleges` b, `users` c
+                                        WHERE b.`id` = :id
+                                        AND a.`user_id` = c.`id`
+                                        AND b.`collegekey` = :user_code
+                                        AND a.`topicable_id` = b.`id` 
+                                        AND a.`topicable_type` like  '%College%' 
+                                        ORDER BY a.`updated_at` DESC
+                                        limit 10", ['id' => $id, 'user_code' => $user_code]);
+
+        return view('viewprofiledoctor', compact( 'topics' ,'id', 'user_code', 'categorytype', 'name'  , 'address' , 'city' , 'country', 'speciality', 'type','qualification' , 'exp'));
 
     }
 
@@ -60,13 +80,20 @@ class CategoryController extends Controller
     	$user_code = $url;
         $categorytype = "Companies";
 
-        $user = Company::where('companykey','=',$url)->first(['id','companykey AS user_code', 'name']);
+        $user = Company::where('companykey','=',$url)->first(['id','companykey AS user_code', 'name' , 'city', 'country' , 'address' ]);
 
         $topicable = "App\Company";
-
+ 
 
         $id = $user->id;
         $name = $user->name;
+        $address = $user->address;
+        $city = $user->city;
+        $country = $user->country;  
+        $speciality =  "";
+        $type = "";
+        $qualification =  "";
+        $exp =  "";
          
         $topic = ShowTopicCategory::where('topicable_type', '=', $topicable)->where('topicable_id', '=', $id)->first(['id']);
 
@@ -87,7 +114,20 @@ class CategoryController extends Controller
 
         }
 
-        return view('viewprofiledoctor', compact('id', 'user_code', 'categorytype', 'name'));
+        $topics = DB::select("SELECT  a.`id`,  a.`url` , a.`user_id`,  a.`topic_name`
+                                    , DATE_FORMAT(a.`created_at`, '%d-%b-%Y')  created_at ,
+                                    (case when (a.`anonymous` = 0) then c.`user_code` else '-1' end) as user_code,
+                                    (case when (a.`anonymous` = 0) then c.`name` else '-1' end) as user_name
+                                FROM `topic_categories` a ,  `companies` b, `users` c
+                                        WHERE b.`id` = :id
+                                        AND a.`user_id` = c.`id`
+                                        AND b.`companykey` = :user_code
+                                        AND a.`topicable_id` = b.`id` 
+                                        AND a.`topicable_type` like  '%Company%' 
+                                        ORDER BY a.`updated_at` DESC
+                                        limit 10", ['id' => $id, 'user_code' => $user_code]);
+
+        return view('viewprofiledoctor', compact( 'topics' ,'id', 'user_code', 'categorytype', 'name', 'address' , 'city' , 'country', 'speciality', 'type','qualification' , 'exp'));
 
     }
 
@@ -152,13 +192,20 @@ class CategoryController extends Controller
     	$user_code = $url;
         $categorytype = "fitnesscenters";
 
-        $user = FitnessCenter::where('fitnesscenterkey','=',$url)->first(['id','fitnesscenterkey AS user_code', 'name']);
+        $user = FitnessCenter::where('fitnesscenterkey','=',$url)->first(['id','fitnesscenterkey AS user_code', 'name', 'city', 'country' , 'address' ]);
 
         $topicable = "App\FitnessCenter";
 
 
         $id = $user->id;
         $name = $user->name;
+        $address = $user->address;
+        $city = $user->city;
+        $country = $user->country;  
+        $speciality =  "";
+        $type = "";
+        $qualification =  "";
+        $exp =  "";
          
         $topic = ShowTopicCategory::where('topicable_type', '=', $topicable)->where('topicable_id', '=', $id)->first(['id']);
 
@@ -179,7 +226,20 @@ class CategoryController extends Controller
 
         }
 
-        return view('viewprofiledoctor', compact('id', 'user_code', 'categorytype', 'name'));
+        $topics = DB::select("SELECT  a.`id`,  a.`url` , a.`user_id`,  a.`topic_name`
+                                    , DATE_FORMAT(a.`created_at`, '%d-%b-%Y')  created_at ,
+                                    (case when (a.`anonymous` = 0) then c.`user_code` else '-1' end) as user_code,
+                                    (case when (a.`anonymous` = 0) then c.`name` else '-1' end) as user_name
+                                FROM `topic_categories` a ,  `fitness_centers` b, `users` c
+                                        WHERE b.`id` = :id
+                                        AND a.`user_id` = c.`id`
+                                        AND b.`fitnesscenterkey` = :user_code
+                                        AND a.`topicable_id` = b.`id` 
+                                        AND a.`topicable_type` like  '%Fitness%' 
+                                        ORDER BY a.`updated_at` DESC
+                                        limit 10", ['id' => $id, 'user_code' => $user_code]);
+
+        return view('viewprofiledoctor', compact( 'topics' ,'id', 'user_code', 'categorytype' , 'name', 'address' , 'city' , 'country', 'speciality', 'type','qualification' , 'exp'));
 
     }
 
@@ -188,13 +248,20 @@ class CategoryController extends Controller
     	$user_code = $url;
         $categorytype = "Hotels";
 
-        $user = Hotel::where('hotelkey','=',$url)->first(['id','hotelkey AS user_code', 'name']);
+        $user = Hotel::where('hotelkey','=',$url)->first(['id','hotelkey AS user_code', 'name',  'city', 'country' , 'address']);
 
         $topicable = "App\Hotel";
 
 
         $id = $user->id;
-        $name = $user->name;
+        $name = $user->name; 
+        $address = $user->address;
+        $city = $user->city;
+        $country = $user->country;  
+        $speciality =  "";
+        $type = "";
+        $qualification =  "";
+        $exp =  "";
          
         $topic = ShowTopicCategory::where('topicable_type', '=', $topicable)->where('topicable_id', '=', $id)->first(['id']);
 
@@ -215,7 +282,20 @@ class CategoryController extends Controller
 
         }
 
-        return view('viewprofiledoctor', compact('id', 'user_code', 'categorytype', 'name'));
+        $topics = DB::select("SELECT  a.`id`,  a.`url` , a.`user_id`,  a.`topic_name`
+                                    , DATE_FORMAT(a.`created_at`, '%d-%b-%Y')  created_at ,
+                                    (case when (a.`anonymous` = 0) then c.`user_code` else '-1' end) as user_code,
+                                    (case when (a.`anonymous` = 0) then c.`name` else '-1' end) as user_name
+                                FROM `topic_categories` a ,  `hotels` b, `users` c
+                                        WHERE b.`id` = :id
+                                        AND a.`user_id` = c.`id`
+                                        AND b.`hotelkey` = :user_code
+                                        AND a.`topicable_id` = b.`id` 
+                                        AND a.`topicable_type` like  '%Hotel%' 
+                                        ORDER BY a.`updated_at` DESC
+                                        limit 10", ['id' => $id, 'user_code' => $user_code]);
+
+        return view('viewprofiledoctor', compact( 'topics' ,'id', 'user_code', 'categorytype', 'name', 'address' , 'city' , 'country', 'speciality', 'type','qualification' , 'exp'));
 
     }
 
@@ -224,13 +304,20 @@ class CategoryController extends Controller
     	$user_code = $url;
         $categorytype = "Lawyers";
 
-        $user = Lawyer::where('lawyerkey','=',$url)->first(['id','lawyerkey AS user_code', 'name']);
+        $user = Lawyer::where('lawyerkey','=',$url)->first(['id','lawyerkey AS user_code', 'name',  'city', 'country' , 'address']);
 
         $topicable = "App\Lawyer";
 
 
         $id = $user->id;
         $name = $user->name;
+        $address = $user->address;
+        $city = $user->city;
+        $country = $user->country;  
+        $speciality =  "";
+        $type = "";
+        $qualification =  "";
+        $exp =  "";
          
         $topic = ShowTopicCategory::where('topicable_type', '=', $topicable)->where('topicable_id', '=', $id)->first(['id']);
 
@@ -251,7 +338,20 @@ class CategoryController extends Controller
 
         }
 
-        return view('viewprofiledoctor', compact('id', 'user_code', 'categorytype', 'name'));
+        $topics = DB::select("SELECT  a.`id`,  a.`url` , a.`user_id`,  a.`topic_name`
+                                    , DATE_FORMAT(a.`created_at`, '%d-%b-%Y')  created_at ,
+                                    (case when (a.`anonymous` = 0) then c.`user_code` else '-1' end) as user_code,
+                                    (case when (a.`anonymous` = 0) then c.`name` else '-1' end) as user_name
+                                FROM `topic_categories` a ,  `lawyers` b, `users` c
+                                        WHERE b.`id` = :id
+                                        AND a.`user_id` = c.`id`
+                                        AND b.`lawyerkey` = :user_code
+                                        AND a.`topicable_id` = b.`id` 
+                                        AND a.`topicable_type` like  '%Lawyer%' 
+                                        ORDER BY a.`updated_at` DESC
+                                        limit 10", ['id' => $id, 'user_code' => $user_code]);
+
+        return view('viewprofiledoctor', compact( 'topics' , 'id', 'user_code', 'categorytype', 'name', 'address' , 'city' , 'country', 'speciality', 'type','qualification' , 'exp'));
 
     }
 
@@ -260,13 +360,20 @@ class CategoryController extends Controller
     	$user_code = $url;
         $categorytype = "Restaurants";
 
-        $user = Restaurant::where('restaurantkey','=',$url)->first(['id','restaurantkey AS user_code', 'name']);
+        $user = Restaurant::where('restaurantkey','=',$url)->first(['id','restaurantkey AS user_code', 'name',  'city', 'country' , 'address']);
 
             $topicable = "App\Restaurant";
 
 
         $id = $user->id;
         $name = $user->name;
+        $address = $user->address;
+        $city = $user->city;
+        $country = $user->country;  
+        $speciality =  "";
+        $type = "";
+        $qualification =  "";
+        $exp =  "";
          
         $topic = ShowTopicCategory::where('topicable_type', '=', $topicable)->where('topicable_id', '=', $id)->first(['id']);
 
@@ -287,7 +394,20 @@ class CategoryController extends Controller
 
         }
 
-        return view('viewprofiledoctor', compact('id', 'user_code', 'categorytype', 'name'));
+        $topics = DB::select("SELECT  a.`id`,  a.`url` , a.`user_id`,  a.`topic_name`
+                                    , DATE_FORMAT(a.`created_at`, '%d-%b-%Y')  created_at ,
+                                    (case when (a.`anonymous` = 0) then c.`user_code` else '-1' end) as user_code,
+                                    (case when (a.`anonymous` = 0) then c.`name` else '-1' end) as user_name
+                                FROM `topic_categories` a ,  `restaurants` b, `users` c
+                                        WHERE b.`id` = :id
+                                        AND a.`user_id` = c.`id`
+                                        AND b.`restaurantkey` = :user_code
+                                        AND a.`topicable_id` = b.`id` 
+                                        AND a.`topicable_type` like  '%Restaurant%' 
+                                        ORDER BY a.`updated_at` DESC
+                                        limit 10", ['id' => $id, 'user_code' => $user_code]);
+
+        return view('viewprofiledoctor', compact( 'topics' ,'id', 'user_code', 'categorytype', 'name', 'address' , 'city' , 'country', 'speciality', 'type','qualification' , 'exp'));
 
     }
 
@@ -296,13 +416,20 @@ class CategoryController extends Controller
     	$user_code = $url;
         $categorytype = "Schools";
 
-        $user = School::where('schoolkey','=',$url)->first(['id','schoolkey AS user_code' , 'name']);
+        $user = School::where('schoolkey','=',$url)->first(['id','schoolkey AS user_code' , 'name',  'city', 'country' , 'address']);
 
         $topicable = "App\School";
 
 
         $id = $user->id;
         $name = $user->name;
+        $address = $user->address;
+        $city = $user->city;
+        $country = $user->country;  
+        $speciality =  "";
+        $type = "";
+        $qualification =  "";
+        $exp =  "";
          
         $topic = ShowTopicCategory::where('topicable_type', '=', $topicable)->where('topicable_id', '=', $id)->first(['id']);
 
@@ -323,7 +450,21 @@ class CategoryController extends Controller
 
         }
 
-        return view('viewprofiledoctor', compact('id', 'user_code', 'categorytype', 'name'));
+        $topics = DB::select("SELECT  a.`id`,  a.`url` , a.`user_id`,  a.`topic_name`
+                                    , DATE_FORMAT(a.`created_at`, '%d-%b-%Y')  created_at ,
+                                    (case when (a.`anonymous` = 0) then c.`user_code` else '-1' end) as user_code,
+                                    (case when (a.`anonymous` = 0) then c.`name` else '-1' end) as user_name
+                                FROM `topic_categories` a ,  `schools` b, `users` c
+                                        WHERE b.`id` = :id
+                                        AND a.`user_id` = c.`id`
+                                        AND b.`schoolkey` = :user_code
+                                        AND a.`topicable_id` = b.`id` 
+                                        AND a.`topicable_type` like  '%School%' 
+                                        ORDER BY a.`updated_at` DESC
+                                        limit 10", ['id' => $id, 'user_code' => $user_code]);
+
+
+        return view('viewprofiledoctor', compact( 'topics' ,'id', 'user_code', 'categorytype', 'name', 'address' , 'city' , 'country', 'speciality', 'type','qualification' , 'exp'));
 
     }
 }
